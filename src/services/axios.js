a2e542ec72c0309a0cd862da7db355c022a0241e
yosiadavida <119830385+yosiadavida@users.noreply.github.com>
2022-12-18 15:37:04 +0700

    search implement

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,17 @@
 import axios from 'axios'
 
+const API_KEY = "bbd4cbf30b4db1252182844eded615bf";
+const URLbase = "https://api.themoviedb.org/3"
+
 const urlRegister = axios.create({
     baseURL: 'https://api.themoviedb.org/3'
 })
+
+export const searchMovie = async (q) => {
+    const search = await axios.get(
+        `${URLbase}/search/movie?query=${q}&api_key=${API_KEY}`
+    )
+    return search.data
+}
 
 export default urlRegister
